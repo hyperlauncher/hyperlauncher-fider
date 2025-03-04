8e590d7ea0dd58d660d1d46f3b43bffa0623cf8f
Gull Tech <gull.superlauncher@gmail.com>
2025-03-04 09:58:42 +0700

    Upđate Privy client config

--- public/services/privy.ts
+++ public/services/privy.ts
@@ -1,7 +1,7 @@
 import { PrivyClientConfig } from "@privy-io/react-auth"
 
 export const privyConfig: PrivyClientConfig = {
-  loginMethods: ["email", "wallet", "google", "twitter"],
+  loginMethods: ["email", "wallet", "google"],
   appearance: {
     loginMessage: "Welcome to Hyperlauncher.ai",
     theme: "light",
